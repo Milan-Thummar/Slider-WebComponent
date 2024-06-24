ff43ab41e255aa615347b67d3b2a7236ce1792a0
Milan Thummar <thummar.fb@gmail.com>
2024-06-24 22:11:12 +0200

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
@@ -22,5 +26,38 @@ module.exports = (env, argv) => {
         },
       ],
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/slider.html",
+      }),
+      ...(isProduction
+        ? [
+            new MiniCssExtractPlugin({
+              filename: "slider.css",
+            }),
+          ]
+        : []),
+    ],
+    optimization: {
+      minimize: isProduction,
+      minimizer: isProduction
+        ? [
+            new TerserPlugin({
+              parallel: true,
+            }),
+            new CssMinimizerPlugin({
+              parallel: true,
+            }),
+          ]
+        : [],
+    },
+    devServer: isProduction
+      ? undefined
+      : {
+          static: {
+            directory: path.join(__dirname, "public"),
+          },
+          open: true,
+        },
   };
 };
